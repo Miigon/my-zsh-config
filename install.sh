6f131270635281fc9294cdea3dbce22d2097865b
miigon <miigon.pg@gmail.com>
2024-03-13 10:51:47 +0800

    remove localrc, add zshrc_real, put zshrc in gitignore instead
    local rc now should be directly written in zshrc
    this change is to make scripts that adds stuff to zshrc work better

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ if [[ "$PWD" != "$HOME/dotfiles/zsh" ]]; then
 	exit 1
 fi
 
+echo "Copy zshrc_stub to zshrc if it doesn't exist"
+[ ! -f ./zshrc ] && cp zshrc_stub zshrc
+
 echo Clone antidote
 git clone --depth=1 https://github.com/mattmc3/antidote.git antidote
 
